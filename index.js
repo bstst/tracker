38e55a5789d2c1539bd82859836682d1ed0361ef
Martin Saulis <martin@saulis.com>
2023-04-05 13:09:23 +0300

    update method signature

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const track = (data) => {
+const track = (id, data) => {
+  if (!id) {
+    return Promise.reject("id required");
+  }
+  if (!data) {
+    return Promise.reject("data required");
+  }
   try {
     return fetch("https://tracker.saulis.com/track", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({ id, data }),
     });
   } catch (e) {
     console.log(e);
